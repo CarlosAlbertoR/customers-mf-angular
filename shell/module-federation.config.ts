6361873fcc0f1157fe45b8b1b36a883acca7bdec
CRDEV <carlosalbertorios3@gmail.com>
2025-10-27 01:30:14 -0500

    chore: Update customers microfrontend URL format in module federation configuration
    - Modified the customers microfrontend URL in the module federation configuration to include a prefix for better clarity in development environments.

--- shell/module-federation.config.ts
+++ shell/module-federation.config.ts
@@ -9,7 +9,7 @@ const customersMFUrl = isDevelopment
 export default createModuleFederationConfig({
   name: "shell",
   remotes: {
-    customersMF: customersMFUrl,
+    customersMF: `customersMF@${customersMFUrl}`,
   },
   shared: {
     react: {
